4542cb8655c6dad7a633d17123722d45688ddbcf
Roman Tyniv <roman.tyniv@3ss.tv>
2023-03-14 10:40:28 +0200

    navigation props

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,17 @@
-import { View, Text, FlatList, StyleSheet } from "react-native";
-import { MEALS } from "../data/dummy-data";
+import { useEffect, useLayoutEffect } from "react";
+import { View, FlatList, StyleSheet } from "react-native";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
 
-const MealsOverviewScreen = ({ route }) => {
+const MealsOverviewScreen = ({ route, navigation }) => {
     const catId = route.params.categoryId;
-    const displayedMeals = MEALS.filter((mealItem) => mealItem.categoryIds.indexOf(catId) > 0);
+    const displayedMeals = MEALS.filter((mealItem) => (mealItem.categoryIds.indexOf(catId) >= 0));
+
+    useLayoutEffect(() => {
+        const categoryTitle = CATEGORIES.find((category) => category.id === catId)?.title;
+        navigation.setOptions({ title: categoryTitle });
+    }, [catId, navigation]);
+
 
     const renderMealItem = (itemData) => {
         const item = itemData.item;
