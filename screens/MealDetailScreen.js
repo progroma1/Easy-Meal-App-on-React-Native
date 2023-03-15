8f781ca1c4f0831f8c0b71b666adde9baefc3644
Roman Tyniv <roman.tyniv@3ss.tv>
2023-03-15 17:59:43 +0200

    more components in meal detail screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,8 @@
 
 import { View, Text, Image, StyleSheet } from "react-native";
 import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/MealDetail/Subtitle";
+import List from "../components/MealDetail/List";
 import { MEALS } from '../data/dummy-data';
 
 
@@ -10,15 +12,18 @@ const MealDetailScreen = ({ route }) => {
 
     return (
         <View>
-            <Image source={{ uri: selectedMeal.imageUrl }} />
+            <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
             <Text>{selectedMeal.title}</Text>
             <MealDetails 
                 duration={selectedMeal.duration}
                 affordability={selectedMeal.affordability}
                 complexity={selectedMeal.complexity}
+                textStyle={styles.detailText}
             />
-            <Text>Ingredients</Text>
-            <Text>Steps</Text>
+            <Subtitle>Ingredients</Subtitle>
+            <List data={selectedMeal.ingredients} />
+            <Subtitle>Steps</Subtitle>
+            <List data={selectedMeal.steps} />
         </View>
     );
 };
@@ -26,5 +31,18 @@ const MealDetailScreen = ({ route }) => {
 export default MealDetailScreen;
 
 const styles = StyleSheet.create({
-
+    image: {
+        width: '100%',
+        height: 350,
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        margin: 8,
+        textAlign: 'center',
+        color: 'white',
+    },
+    detailText: {
+        color: 'white',
+    },
 });
