732dfcecb76dd56c89853b516957d11c818fdf92
Roman Tyniv <roman.tyniv@3ss.tv>
2023-03-20 15:47:50 +0200

    finishing with context

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,13 +1,37 @@
 import { useContext } from 'react';
+import { View, Text, StyleSheet } from "react-native";
 import MealsList from '../components/MealsList/MealsList';
 import { FavoritesContext } from '../store/context/favorites-context';
 import { MEALS } from "../data/dummy-data";
 
 const FavoritesScreen = () => {
   const favoriteMealsCtx = useContext(FavoritesContext);
-  const displayedMeals = MEALS.filter((mealItem) => favoriteMealsCtx.ids.includes(mealItem.id));
-  
-  return <MealsList items={displayedMeals} />;
+  const favoriteMeals = MEALS.filter((mealItem) => favoriteMealsCtx.ids.includes(mealItem.id));
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>You have no favorites meals yet!</Text>
+      </View>
+    );
+  }
+
+  return <MealsList items={favoriteMeals} />;
 };
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      padding: 16,
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center',
+  },
+});
